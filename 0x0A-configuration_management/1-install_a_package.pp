2335232d2996f7e567c3d46f4efda62e903ac8b4
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-30 15:14:39 +0100

    add:install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,7 @@ package { 'python3-pip':
 }
 
 exec {'install_flask':
-  command   => 'pip3 install flask==2.1.0',
+  command  => 'pip3 install flask==2.1.0',
   require  => Package['python3-pip'],
-  unless => 'pip3 show flask | grep -q "Version: 2.1.0"',
+  unless   => 'pip3 show flask | grep -q "Version: 2.1.0"',
 }
