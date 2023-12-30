73d6fefa58718a5c13eb33c029c07f9df9a921ed
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-30 14:58:21 +0100

    add:100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,17 @@
 #!/usr/bin/pup
-# creates a file school, mode, owner, group & content
-file { '~/.ssh/school':
-  path    => '~/.ssh/school',
-  content => "Host 181650-web-01
- HostName 54.175.13.73
- User ubuntu
- IdentityFile ~/.ssh/school
- IdentityOnly yes"
+# setting up my client config file
+include stdlib
+
+file_line { 'Turn off password auth':
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => '    PasswordAuthentication no',
+  replace  => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
 }
---
+++ 0x0B-ssh/100-puppet_ssh_config.pp~
@@ -0,0 +1,10 @@
+#!/usr/bin/pup
+# creates a file school, mode, owner, group & content
+file { '~/.ssh/school':
+  path    => '~/.ssh/school',
+  content => "Host 181650-web-01
+ HostName 54.175.13.73
+ User ubuntu
+ IdentityFile ~/.ssh/school
+ IdentityOnly yes"
+}
